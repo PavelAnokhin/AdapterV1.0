5e3662f3c663adc1896d9efdc12bc50b47027cf2
PavelAnokhin <pav-anokhin@yandex.ru>
2021-09-14 18:06:11 +0300

    Initial first commit for V1.0

---
+++ src/main/java/ru/netology/IntsCalculator.java
@@ -0,0 +1,18 @@
+package ru.netology;
+
+public class IntsCalculator implements Ints {
+    @Override
+    public int sum(int arg0, int arg1) {
+        return 0;
+    }
+
+    @Override
+    public int mult(int arg0, int arg1) {
+        return 0;
+    }
+
+    @Override
+    public int pow(int a, int b) {
+        return 0;
+    }
+}
